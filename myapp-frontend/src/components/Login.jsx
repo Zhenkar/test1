19a2fdb6a86081af4dc494cafee913493868b274
Zhenkar Gowda K P <151987799+Zhenkar@users.noreply.github.com>
2025-10-16 09:08:59 +0530

    Updated login

--- myapp-frontend/src/components/Login.jsx
+++ myapp-frontend/src/components/Login.jsx
@@ -27,7 +27,7 @@ export const Login = () => {
       setErrors({}); // Clear errors
 
       // Make the API call
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
